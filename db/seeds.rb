0bec04ee5d98fbe5a49a41ef2c35d76fee5c2e76
samuel.shaw <samuel.shaw@covermymeds.com>
2022-03-16 15:07:22 -0400

    working api hit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create([
+                 {
+                   name: "thing 1",
+                   brand: "off broadway",
+                   price: "two dollas",
+                   description: "totes a real thing"
+                 },
+                 {
+                   name: "thing 2",
+                   brand: "off broadway",
+                   price: "four dollas",
+                   description: "totes not a real thing"
+                 },
+                 {
+                   name: "catz",
+                   brand: "broadway",
+                   price: "two hundo dollos",
+                   description: "the non butthole edition"
+                 },
+                 {
+                   name: "i'm bored",
+                   brand: "sam",
+                   price: "free",
+                   description: "save me"
+                 }
+               ])
